634898cafba7478f680f96a2b517525d8fffc1b3
hshy1839 <hongjeongmin1839@gmail.com>
2024-05-20 23:15:11 +0900

    게시판 get 가능 + postDetail 파라미터 불러오기

--- components/Post.js
+++ components/Post.js
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity, TextInput } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
 import Footer from './Footer';
 import axios from 'axios';
+import { useNavigation } from '@react-navigation/native';
 
 const DisplayPosts = () => {
   const [posts, setPosts] = useState([]);
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const navigation = useNavigation();
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -19,16 +23,63 @@ const DisplayPosts = () => {
     fetchPosts();
   }, []);
 
+  const handlePostDetail = (post) => {
+    const date = new Date(post.created_at);
+    const formattedDate = date.toISOString().split('T')[0];
+    navigation.navigate('PostDetail', { 
+      title: post.title,
+      content: post.content,
+      username: post.username,
+      created_at: formattedDate
+    });
+  };
+
+  // 게시물 필터링 함수
+  const filterPosts = (searchKeyword) => {
+    return posts.filter((post) => {
+      return post.title.toLowerCase().includes(searchKeyword.toLowerCase()) || post.content.toLowerCase().includes(searchKeyword.toLowerCase());
+    });
+  };
+
+  // 검색 실행 함수
+  const handleSearch = async () => {
+    if (!searchKeyword) {
+      try {
+        const response = await axios.get('http://172.16.2.102:3000/api/buddy/getboard');
+        setPosts(response.data);
+      } catch (error) {
+        console.error('게시물을 불러오는 데 실패했습니다:', error);
+      }
+      return;
+    }
+    
+    const filteredPosts = filterPosts(searchKeyword);
+    setPosts(filteredPosts);
+  };
+
   return (
     <View style={styles.container}>
+      <View style={styles.searchBar}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="검색어를 입력하세요"
+          onChangeText={(text) => setSearchKeyword(text)}
+          value={searchKeyword}
+        />
+        <TouchableOpacity style={styles.searchButton} onPress={handleSearch}>
+          <FontAwesome5 name="search" size={20} color="white" />
+        </TouchableOpacity>
+      </View>
       <ScrollView style={styles.scrollContainer}>
         {posts.map((post, index) => (
-          <View key={index} style={styles.postContainer}>
+          <TouchableOpacity activeOpacity={0.8} key={index} style={styles.postContainer} onPress={() => handlePostDetail(post)}>
             <Text style={styles.title}>{post.title}</Text>
             <Text style={styles.content}>{post.content}</Text>
-            <Text style={styles.author}>작성자: {post.username}</Text>
-            <Text style={styles.date}>날짜: {post.created_at}</Text>
-          </View>
+            <View style={styles.subInfo}>
+            <Text style={styles.author}>게시자 : {post.username}</Text>
+            <Text style={styles.date}> {new Date(post.created_at).toLocaleDateString()}</Text>
+            </View>
+          </TouchableOpacity>
         ))}
       </ScrollView>
       <Footer />
@@ -41,30 +92,64 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fafad2",
   },
-  scrollContainer: {
-    maxHeight: '90%',
+  searchBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingTop: 10,
+    paddingBottom: 5,
+    borderBottomWidth : 1.2,
+    borderColor : 'black',
   },
-  postContainer: {
-    marginBottom: 20,
-    padding: 20,
-    backgroundColor: "white",
-    borderWidth: 1.5,
+  searchInput: {
+    flex: 1,
+    borderWidth: 1,
     borderColor: 'black',
     borderRadius: 5,
+    paddingVertical: 8,
+    paddingHorizontal: 10,
+    marginRight: 10,
+    backgroundColor : 'white',
+  },
+  searchButton: {
+    padding: 10,
+    backgroundColor: '#0095f6',
+    borderRadius: 5,
+  },
+  scrollContainer: {
+    maxHeight: '85%',
+  },
+  postContainer: {
+    padding: 5,
+    backgroundColor: "white",
+    borderWidth: 1,
+    borderColor: 'black',
+    borderTopWidth : 0,
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontFamily: 'SpaceGroteskBold',
     marginBottom: 10,
   },
   content: {
-    fontSize: 20,
+    fontSize: 16,
     marginBottom: 10,
+    fontFamily : 'SpaceGroteskRegular',
   },
   author: {
-    fontSize: 18,
+    fontSize: 16,
     color: 'gray',
+    fontFamily: 'SpaceGroteskBold',
   },
+  date: {
+    fontSize: 16,
+    color: 'gray',
+    marginLeft : 'auto',
+    fontFamily: 'SpaceGroteskBold',
+  },
+  subInfo : {
+    flexDirection : 'row',
+  }
 });
 
 export default DisplayPosts;
--- components/PostDetail.js
+++ components/PostDetail.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Post from './Post';
 
 const PostDetailScreen = ({ route }) => {
-    // 이전 화면에서 전달된 게시물 id를 받아옵니다.
-    const { postId, title, content } = route.params;
+    // 이전 화면에서 전달된 게시물 정보를 받아옵니다.
+    const { title, content, username, created_at } = route.params;
 
     return (
         <View style={styles.container}>
+            <View style={styles.postContainer}>
+            <Text style={styles.author}>{username}</Text>
+            <Text style={styles.date}>{new Date(created_at).toLocaleDateString()}</Text>
             <Text style={styles.title}>{title}</Text>
             <Text style={styles.content}>{content}</Text>
+            </View>
         </View>
     );
 };
@@ -18,7 +21,10 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 20,
-        backgroundColor: 'white',
+        backgroundColor: '#fafad2',
+    },
+    postContainer : {
+        backgroundColor : 'white',
     },
     title: {
         fontSize: 24,
@@ -27,6 +33,15 @@ const styles = StyleSheet.create({
     },
     content: {
         fontSize: 16,
+    },
+    author: {
+        fontSize: 18,
+        color: 'gray',
+        marginBottom: 5,
+    },
+    date: {
+        fontSize: 16,
+        color: 'gray',
     },
 });
 
